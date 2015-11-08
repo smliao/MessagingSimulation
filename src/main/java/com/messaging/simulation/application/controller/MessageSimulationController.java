e81d612008c60d5de4129e3eee1a3a8757281f33
St2s6b6 <steven_liao@gap.com>
2015-11-08 11:39:04 -0800

    Add logs to MessageSimulationController

--- src/main/java/com/messaging/simulation/application/controller/MessageSimulationController.java
+++ src/main/java/com/messaging/simulation/application/controller/MessageSimulationController.java
@@ -5,6 +5,7 @@ import com.messaging.simulation.application.service.MessageSimulationService;
 import com.messaging.simulation.application.view.MessageSimulationRequestResource;
 import com.messaging.simulation.application.view.MessageSimulationResponseResource;
 import com.messaging.simulation.domain.MessageSimulation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping(value = MessageSimulationPaths.ROOT_PATH, method = RequestMethod.GET)
 public class MessageSimulationController {
@@ -27,15 +29,22 @@ public class MessageSimulationController {
 
     @RequestMapping(value = MessageSimulationPaths.TEST_PATH, method = RequestMethod.GET)
     public ResponseEntity<String> testingSpringBoot() {
+
+        log.info("Hello World Spring Boot");
+
         return new ResponseEntity<>("Hello World Spring Boot", HttpStatus.OK);
     }
 
     @RequestMapping(value = MessageSimulationPaths.CHAT, method = RequestMethod.POST)
-    public ResponseEntity<Map<String, Object>> create(@RequestBody MessageSimulationRequestResource messageSimulationRequestResource){
+    public ResponseEntity<Map<String, Object>> create(@RequestBody MessageSimulationRequestResource messageSimulationRequestResource) {
+
+        log.info("Create new chat {}", messageSimulationRequestResource);
 
         MessageSimulation messageSimulation = messageSimulationRequestResource.toDomain();
 
         MessageSimulation createdMessageSimulation = messageSimulationService.create(messageSimulation);
+
+        log.info("Result {} saved in messageSimulationRepository", createdMessageSimulation);
 
         Map<String, Object> result = MessageSimulationResponseResource.toCreateResource(createdMessageSimulation);
 
@@ -43,19 +52,29 @@ public class MessageSimulationController {
     }
 
     @RequestMapping(value = MessageSimulationPaths.ID, method = RequestMethod.GET)
-    public ResponseEntity<MessageSimulationResponseResource> findOne(@PathVariable String id){
+    public ResponseEntity<MessageSimulationResponseResource> findOne(@PathVariable String id) {
+
+        log.info("Searching for chat on id: {}", id);
 
         MessageSimulation messageSimulation = messageSimulationService.findById(id);
 
+        log.info("Searched chat for id {} returned {}", id, messageSimulation);
+
         MessageSimulationResponseResource result = MessageSimulationResponseResource.toResource(messageSimulation);
+
+        log.info("MessageSimulationResponseResource result: {}", result);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @RequestMapping(value = MessageSimulationPaths.USER_NAME, method = RequestMethod.GET)
-    public ResponseEntity<Collection<Map<String, Object>>> findByUsername(@PathVariable String username){
+    public ResponseEntity<Collection<Map<String, Object>>> findByUsername(@PathVariable String username) {
+
+        log.info("Find all chat with username : {}", username);
 
         List<MessageSimulation> messageSimulationList = messageSimulationService.findByUsername(username);
+
+        log.info("Result of all {} chats: {}", username, messageSimulationList);
 
         List<Map<String, Object>> result = MessageSimulationResponseResource.toListResource(messageSimulationList);
 
