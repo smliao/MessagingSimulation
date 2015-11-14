cdcae73486c0683c721f047ef9e7cf79208154e8
St2s6b6 <steven_liao@gap.com>
2015-11-13 18:34:37 -0800

    Refactor MessageSimulationPaths to be a class instead of interface

--- src/main/java/com/messaging/simulation/application/controller/path/MessageSimulationPaths.java
+++ src/main/java/com/messaging/simulation/application/controller/path/MessageSimulationPaths.java
@@ -1,9 +1,9 @@
 package com.messaging.simulation.application.controller.path;
 
-public interface MessageSimulationPaths {
-    String ROOT_PATH = "/simulation";
-    String TEST_PATH = "/";
-    String CHAT = "/chat";
-    String ID = CHAT + "/{id}";
-    String USER_NAME = "/chats/{username}";
+public final class MessageSimulationPaths {
+    public static final String ROOT_PATH = "/simulation";
+    public static final String TEST_PATH = "/";
+    public static final String CHAT = "/chat";
+    public static final String ID = CHAT + "/{id}";
+    public static final String USER_NAME = "/chats/{username}";
 }
